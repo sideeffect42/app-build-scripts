4fd7c557d323fba65a77558bb6e48e674c02f8c4
Takashi Yoshi <takashi@yoshi.email>
2017-01-26 20:53:04 +0100

    Added build script for jq

---
+++ jq/mkjq.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+APPLICATION='jq'
+VERSION=1.5
+
+
+set -e -x
+pushd -n "$(pwd)"
+
+DIR="$(dirname "$0")"
+
+cd "${DIR}/${APPLICATION}-${VERSION}"
+
+
+# === configure ===
+
+autoreconf -fiv
+./configure --prefix="$HOME/app/${APPLICATION}" \
+	--sysconfdir="$HOME/etc/${APPLICATION}" \
+	 --disable-valgrind --enable-all-static --enable-static
+
+# === make ===
+
+make
+make install
+
+# === package ===
+
+TAR_OUT="${DIR}/tar/${APPLICATION}-${VERSION}-$(date +%Y%m%d).tar.gz"
+mkdir -p "$(dirname "$TAR_OUT")"
+(cd "$HOME" && \
+	find "./app/${APPLICATION}" "./etc/${APPLICATION}" -prune -type d -print0 2>&- \
+	| tar --null -cvzf "$TAR_OUT" --files-from -)
+
+echo "Stored tarball in: ${TAR_OUT}"
+
+popd # restore working directory
