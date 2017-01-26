12ce4bc0824a78ef8a6b9d43c7699a1d442ae734
Takashi Yoshi <takashi@yoshi.email>
2017-01-26 20:54:19 +0100

    Added build script for xz

---
+++ xz/mkxz.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+APPLICATION='xz'
+VERSION=5.2.3
+
+
+set -e -x
+pushd -n "$(pwd)"
+
+DIR="$(dirname "$0")"
+
+cd "${DIR}/${APPLICATION}-${VERSION}"
+
+
+# === configure ===
+
+./configure LDFLAGS='-static' --prefix="$HOME/app/${APPLICATION}" \
+	--sysconfdir="$HOME/etc/${APPLICATION}" \
+	--enable-static=yes --enable-werror
+
+# === make ===
+
+make
+make install
+
+popd
+
+# === package ===
+
+TAR_OUT="${DIR}/tar/${APPLICATION}-${VERSION}-$(date +%Y%m%d).tar.gz"
+mkdir -p "$(dirname "$TAR_OUT")"
+(cd "$HOME" && \
+	find "./app/${APPLICATION}" "./etc/${APPLICATION}" -prune -type d -print0 2>&- \
+	| tar --null -cvzf "$TAR_OUT" --files-from -)
+
+echo "Stored tarball in: ${TAR_OUT}"
+
+popd # restore working directory
